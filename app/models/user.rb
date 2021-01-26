d0421b23d405f896e69e53f855211e665c8a0db8
hiro.iwakiti <iwakitihiro392@gmail.com>
2021-01-26 16:10:40 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }    
-  has_secure_password    
+  has_secure_password 
+  
+  has_many :microposts
 end
+
